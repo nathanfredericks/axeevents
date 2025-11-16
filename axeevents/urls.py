460c299f224e3fa8b72ddbedcd81d911dbfe136d
Nathan Fredericks <nathan@fredericks.dev>
2025-11-16 13:13:48 -0400

    Fix template format error and serve media in prod

--- axeevents/urls.py
+++ axeevents/urls.py
@@ -1,11 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("events.urls")),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
 
 if settings.DEBUG:
